bae8f76514d7c5a2b18592f3f6cba0edc5e4c49f
RikuPy <180808102+RikuPy@users.noreply.github.com>
2025-03-27 03:30:53 -0400

    Add get_listings method and related data models for item listings

--- xivuniversalis/client.py
+++ xivuniversalis/client.py
@@ -1,12 +1,75 @@
+import urllib.parse
+from datetime import datetime
+
 import aiohttp
 
 from xivuniversalis.errors import UniversalisServerError, UniversalisError
-from xivuniversalis.models import DataCenter, World
+from xivuniversalis.models import DataCenter, World, Listing, SaleHistory, ListingResults
 
 
 class UniversalisClient:
     def __init__(self):
         self.endpoint: str = "https://universalis.app/api/v2"
+
+    async def get_listings(
+        self,
+        item_ids: int | list[int],
+        world_dc_region: str,
+        *,
+        listing_limit: int | None = None,
+        history_limit: int = 5,
+        hq_only: bool = False,
+    ) -> ListingResults:
+        if isinstance(item_ids, int):
+            item_ids = [item_ids]
+
+        query_params = {"entries": history_limit}
+        if listing_limit is not None:
+            query_params["listings"] = listing_limit
+        if hq_only:
+            query_params["hq"] = 1
+        query_params = urllib.parse.urlencode(query_params)
+
+        listings_data = await self._request(
+            f"{self.endpoint}/{world_dc_region}/{','.join(map(str, item_ids))}?{query_params}"
+        )
+
+        active = []
+        sale_history = []
+        for listing in listings_data["listings"]:
+            active.append(
+                Listing(
+                    listing_id=int(listing["listingID"]),
+                    updated_at=datetime.fromtimestamp(listing["lastReviewTime"]),
+                    quantity=listing["quantity"],
+                    price_per_unit=listing["pricePerUnit"],
+                    total_price=listing["total"],
+                    tax=listing["tax"],
+                    world_name=listing["worldName"],
+                    world_id=listing["worldID"],
+                    hq=listing["hq"],
+                    is_crafted=listing["isCrafted"],
+                    on_mannequin=listing["onMannequin"],
+                    retainer_id=int(listing["retainerID"]),
+                    retainer_name=listing["retainerName"],
+                    retainer_city=listing["retainerCity"],
+                )
+            )
+
+        for sale in listings_data["recentHistory"]:
+            sale_history.append(
+                SaleHistory(
+                    sold_at=datetime.fromtimestamp(sale["timestamp"]),
+                    quantity=sale["quantity"],
+                    price_per_unit=sale["pricePerUnit"],
+                    total_price=sale["total"],
+                    buyer_name=sale["buyerName"],
+                    world_name=sale["worldName"],
+                    world_id=sale["worldID"],
+                )
+            )
+
+        return ListingResults(active=active, sale_history=sale_history)
 
     async def get_datacenters(self) -> list[DataCenter]:
         """
--- xivuniversalis/models.py
+++ xivuniversalis/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass(kw_only=True)
@@ -11,9 +12,10 @@ class DataCenter:
         region (str): The datacenter's region. (e.g. "North-America", "Japan", "Europe", ...)
         worlds (list['World']): A list of worlds in the datacenter.
     """
+
     name: str
     region: str
-    worlds: list['World']
+    worlds: list["World"]
 
     def __eq__(self, other):
         if isinstance(other, DataCenter):
@@ -23,6 +25,9 @@ class DataCenter:
             return self.name == other
 
         return False
+
+    def __str__(self):
+        return self.name
 
 
 @dataclass(kw_only=True)
@@ -36,6 +41,7 @@ class World:
         datacenter (DataCenter | None): The world's datacenter.
             Only provided when worlds are retrieved via the `UniversalisClient.datacenters` method.
     """
+
     id: int
     name: str
     datacenter: DataCenter | None = None
@@ -48,3 +54,45 @@ class World:
             return self.name == other
 
         return False
+
+    def __str__(self):
+        return self.name
+
+
+@dataclass(kw_only=True, slots=True)
+class Listing:
+    listing_id: int
+    updated_at: datetime
+    quantity: int
+    price_per_unit: int
+    total_price: int
+    tax: int
+    world_id: int
+    world_name: str
+    # creator_name: str  # todo: I don't believe either of these return values anymore, review to be sure
+    # creator_id: int
+    hq: bool
+    is_crafted: bool
+    # materia: list = field(default_factory=list)
+    on_mannequin: bool
+    retainer_id: int
+    retainer_name: str
+    retainer_city: int
+    # seller_id: int  # todo: same as above, I don't believe this data is returned anymore
+
+
+@dataclass(kw_only=True, slots=True)
+class SaleHistory:
+    sold_at: datetime
+    quantity: int
+    price_per_unit: int
+    total_price: int
+    buyer_name: str
+    world_id: int
+    world_name: str
+
+
+@dataclass(kw_only=True, slots=True)
+class ListingResults:
+    active: list[Listing]
+    sale_history: list[SaleHistory]
