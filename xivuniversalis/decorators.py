00c7183a17b7103b0597a26b370116feef0f88b5
Riku <180808102+RikuPy@users.noreply.github.com>
2025-04-09 22:38:11 -0400

    fix: A better solution for #9 that avoids errors when a string is passed into item_ids (#10)

--- xivuniversalis/decorators.py
+++ xivuniversalis/decorators.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from typing import Sequence
 
 __all__ = ["supports_multiple_ids"]
 
@@ -7,7 +8,7 @@ def supports_multiple_ids(func):
     @wraps(func)
     async def wrapper(self, item_ids, *args, **kwargs):
         result = await func(self, item_ids, *args, **kwargs)
-        if isinstance(item_ids, int):
+        if isinstance(item_ids, str) or not isinstance(item_ids, Sequence):
             assert isinstance(result, dict)
             key = next(iter(result))
             return result[key]
